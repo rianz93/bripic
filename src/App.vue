b7a7c5b449236e0417bf0b97551b24708d0f8d2e
rianz93 <ryansupit25@gmail.com>
2021-04-13 01:39:47 +0800

    fix user UI

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <template>
   <div id="app">
-    <div>
-      <b-navbar toggleable="lg" type="dark" variant="info">
-        <b-navbar-nav>
-          <b-nav-item href="#" disabled>
-            <img
-              src="https://www.tokohkita.co/uploads/berita/thumbs/750x400/20190804_bank-bri-pastikan-operasional-bank-tak-terganggu-pasca-pemadaman-listrik.jpg"
-              alt=""
-          /></b-nav-item>
-          <b-nav-item href="#" disabled
-            ><img
-              src="https://faseberita.id/property/files/2020/09/bumn-logo.jpg"
-              alt=""
-          /></b-nav-item>
-        </b-navbar-nav>
-      </b-navbar>
+    <div class=" navbar"">
+      <img 
+        src="https://www.tokohkita.co/uploads/berita/thumbs/750x400/20190804_bank-bri-pastikan-operasional-bank-tak-terganggu-pasca-pemadaman-listrik.jpg"
+        alt=""
+      />
+
+      <img
+        src="https://faseberita.id/property/files/2020/09/bumn-logo.jpg"
+        alt=""
+      />
     </div>
-    
+
     <div style="">
       <router-view></router-view>
     </div>
@@ -35,14 +30,20 @@
 .navbar {
   background-color: #008ed4;
   width: 100%;
-  height: 100px;
+  height: 80px;
   position: absolute;
   z-index: 1;
   box-shadow: 0px 7px 21px 0px rgba(50, 50, 50, 0.1);
 }
 
 .navbar img {
-  height: 80px;
+  height: 60px;
   border-radius: 10px;
+}
+
+body{
+  margin: 0;
+  padding: 0;
+
 }
 </style>
--- src/views/userUI.vue
+++ src/views/userUI.vue
@@ -1,30 +1,210 @@
 <template>
-	<div class="picFile col-12">
+	<div class="picFile">
 		<b-form>
-			<b-form-group
-					label-for="pylon"
-					label="PYLON"
-					label-size="lg"
-				>
+			<div class="mt-2"></div>
+			<div v-for="(data, index) in formdata" class="row pl-4">
+				<b-form-group :label="data.name.toUpperCase()" class="col-9 col-sm-9 ">
 					<b-form-file
 						ref="file"
-						id="pylon"
-						placeholder="Masukkan foto Pylon.."
-						size="lg"
-						@change="onChangeFileSelected($event, pylon)"
+						
+						:id="data.name"
+						:state="data.state"
+						:placeholder="'Masukkan foto ' + data.name + '..'"
+						size="sm"
+						@change="onChangeFileSelected($event, data.id, index)"
 					>
 					</b-form-file>
 				</b-form-group>
+				<div class="preview col-3 col-sm-3 p-1">
+					<i style="font-size: 12px; margin-right: 10px">Preview</i>
+					<img :id="'preview_' + index" >
+				</div>
+			</div>
+			<b-button
+				variant="info"
+				type="submit"
+				class="mb-4 mt-4 btn-block"
+				>Simpan Data <b-icon icon="box-arrow-in-up-right"></b-icon>
+			</b-button>
 		</b-form>
 	</div>
 </template>
 <script>
 export default {
+	created() {
+		this.insertInformation();
+	},
+	methods: {
+		insertInformation() {
+			this.$swal({
+				title: "Masukan Kode Uker",
+				icon: "info",
+				input: "text",
+				confirmButtonText: "Submit",
+				allowOutsideClick: false,
+			}).then((result) => {
+				if (!result.value) {
+					this.$swal({
+						title: "Mohon Untuk Memasukkan Kode Uker",
+						icon: "warning",
+					}).then((result) => {
+						this.insertInformation();
+					});
+				} else {
+					this.kodeUker = result.value;
+					this.$swal("Kode Uker Anda " + result.value, "", "success");
+				}
+			});
+		},
 
+		onChangeFileSelected(event, modelName, index) {
+			let ext = event.target.files[0]["name"].split(".").pop();
+			let reader = new FileReader();
+			reader.onload = () => {
+				let output = document.getElementById("preview_" + index);
+				output.src = reader.result;
+			};
+			reader.readAsDataURL(event.target.files[0]);
+
+			if (ext != "jpg") {
+				this.formdata[index].formatState = false;
+			} else {
+				this.formdata[index].formatState = true;
+				this.formdata[index].state = true;
+			}
+		},
+	},
+	data() {
+		return {
+			kodeUker: "",
+			formdata: [
+				{
+					id: "pylon",
+					name: "pylon",
+					state: false,
+					formatState: true,
+				},
+				{
+					id: "signage",
+					name: "signage",
+					state: false,
+					formatState: true,
+				},
+				{
+					id: "ALP",
+					name: "Area luar parkir",
+					state: false,
+					formatState: true,
+				},
+				{
+					id: "TPDGR",
+					name: "Tampak Depan Gedung RUKO",
+					state: false,
+					formatState: true,
+				},
+				{
+					id: "BH",
+					name: "Banking Hall",
+					state: false,
+					formatState: true,
+				},
+				{
+					id: "AT",
+					name: "Area Teller",
+					state: false,
+					formatState: true,
+				},
+				{
+					id: "AC",
+					name: "Area Cs",
+					state: false,
+					formatState: true,
+				},
+				{
+					id: "RPU",
+					name: "Ruang Pemimpin UKO",
+					state: false,
+					formatState: true,
+				},
+				{
+					id: "RM",
+					name: "Ruang Marketing",
+					state: false,
+					formatState: true,
+				},
+				{
+					id: "RR",
+					name: "Ruang Rapat",
+					state: false,
+					formatState: true,
+				},
+				{
+					id: "RTN",
+					name: "Ruang Tunggu Nasabah",
+					state: false,
+					formatState: true,
+				},
+				{
+					id: "RS",
+					name: "Ruang Server",
+					state: false,
+					formatState: true,
+				},
+				{
+					id: "RA",
+					name: "Ruang Arsip/Dokumen",
+					state: false,
+					formatState: true,
+				},
+				{
+					id: "RP",
+					name: "Ruang Pantry",
+					state: false,
+					formatState: true,
+				},
+				{
+					id: "toilet",
+					name: "Toilet",
+					state: false,
+					formatState: true,
+				},
+				{
+					id: "TDEPANEB",
+					name: "Tampak Depan Gallery E-Banking",
+					state: false,
+					formatState: true,
+				},
+				{
+					id: "TDALAMEB",
+					name: "Tampak Dalam Gallery E-Banking",
+					state: false,
+					formatState: true,
+				},
+				{
+					id: "TB",
+					name: "Ruang Brankas/Kluis",
+					state: false,
+					formatState: true,
+				},
+			],
+		};
+	},
 };
 </script>
 <style>
-	.picfile{
-
-	}
+.picfile {
+}
+.preview img{
+height:90px;
+width:90px;
+}
+.preview {
+	background-color: #f8f5f1;
+	border-radius: 10px;
+	margin-bottom: 8px;
+}
+.errorText {
+	color: red;
+	font-size: 80%;
+}
 </style>
