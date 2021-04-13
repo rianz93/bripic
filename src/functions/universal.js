831c11851a4347c8bbf0df3fbde2c5c3d4594375
rianz93 <ryansupit25@gmail.com>
2021-04-13 14:33:47 +0800

    third commit || IMPROVED PREVIEW

---
+++ src/functions/universal.js
@@ -0,0 +1,1 @@
+export let API_ENDPOINT = "http://localhost/briphoto";
--- src/views/userUI.vue
+++ src/views/userUI.vue
@@ -1,12 +1,14 @@
 <template>
 	<div class="picFile ">
-		<b-form class="p-3">
+		<b-form class="p-3" @submit.prevent="sendDataPost">
 			<div class="mt-2"></div>
 			<div v-for="(data, index) in formdata" class="row ">
-				<b-form-group :label="data.name.toUpperCase()" :class=" !data.state?'col-12 col-sm-12' : 'col-9 col-sm-9' ">
+				<b-form-group
+					:label="data.name.toUpperCase()"
+					:class="!data.state ? 'col-12 col-sm-12' : 'col-9 col-sm-9'"
+				>
 					<b-form-file
 						ref="file"
-						
 						:id="data.name"
 						:state="data.state"
 						:placeholder="'Masukkan foto ' + data.name + '..'"
@@ -14,22 +16,24 @@
 						@change="onChangeFileSelected($event, data.id, index)"
 					>
 					</b-form-file>
+					<div class="errorText mt-1" v-if="!data.formatState">
+						Mohon Untuk Memasukkan File Berformat Image..!
+					</div>
 				</b-form-group>
 				<div class="preview col-3 col-sm-3 p-1" v-if="data.state">
 					<i style="font-size: 12px; margin-right: 10px">Preview</i>
-					<img :id="'preview_' + index" >
+					<img :id="'preview_' + index" />
 				</div>
 			</div>
-			<b-button
-				variant="info"
-				type="submit"
-				class="mb-4 mt-4 btn-block"
+			<b-button variant="info" type="submit" class="mb-4 mt-4 btn-block"
 				>Simpan Data <b-icon icon="box-arrow-in-up-right"></b-icon>
 			</b-button>
 		</b-form>
 	</div>
 </template>
 <script>
+import { API_ENDPOINT } from "../functions/universal.js";	
+const axios = require("axios");
 export default {
 	created() {
 		this.insertInformation();
@@ -39,7 +43,7 @@ export default {
 			this.$swal({
 				title: "Masukan Kode Uker",
 				icon: "info",
-				input: "text",
+				input: "number",
 				confirmButtonText: "Submit",
 				allowOutsideClick: false,
 			}).then((result) => {
@@ -58,24 +62,48 @@ export default {
 		},
 
 		onChangeFileSelected(event, modelName, index) {
-			let ext = event.target.files[0]["name"].split(".").pop();
+			let ext = event.target.files[0].type.split('/')[0];
 			let reader = new FileReader();
 			reader.onload = () => {
 				let output = document.getElementById("preview_" + index);
 				output.src = reader.result;
 			};
 			reader.readAsDataURL(event.target.files[0]);
-
-			if (ext != "jpg") {
+			console.log(ext)
+			if (ext != "image") {
 				this.formdata[index].formatState = false;
 			} else {
 				this.formdata[index].formatState = true;
 				this.formdata[index].state = true;
+
+				this.photoData[modelName] = event.target.files[0];
+				console.log(this.photoData);
 			}
 		},
+
+		sendDataPost(e){
+			let fd = new FormData();
+
+			for(let item in this.photoData){
+				fd.append(item, this.photoData[item]);
+			}
+			fd.append('kodeUker', this.$parent.kodeUker);
+			axios.post(API_ENDPOINT + '/inputPhoto.php', fd,{
+					headers: {
+						"Content-Type": "multipart/form-data",
+					},
+				})
+				.then((response) => {
+					console.log(response.data);
+					if(response.data.status == 'success'){
+						this.$swal('Berhasil','Data anda telah tersimpan','success');
+					}
+				})
+		}
 	},
 	data() {
 		return {
+			photoData:{},
 			formdata: [
 				{
 					id: "pylon",
@@ -193,9 +221,9 @@ export default {
 <style>
 .picfile {
 }
-.preview img{
-height:75px;
-width:75px;
+.preview img {
+	height: 75px;
+	width: 75px;
 }
 .preview {
 	background-color: #f8f5f1;
